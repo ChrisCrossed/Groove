64a5abd367c387d38acd8e2b81f01be73bcdef6a
Chris Christensen <Chriscrossed.86@gmail.com>
2019-01-21 22:13:13 -0800

    Init Setup - Jan 21st, 2019

---
+++ Groove/Assets/cs_BoardManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class cs_BoardManager : MonoBehaviour
+{
+    public enum BlockType
+    {
+        None,
+        Red,
+        Blue
+    }
+    
+    private BlockType[] BlockArray;
+    [SerializeField] int BoardWidth = 7;
+    [SerializeField] int BoardHeight = 9;
+
+    // Use this for initialization
+    void Start()
+    {
+        BlockArray = new BlockType[ BoardHeight * BoardWidth ];
+
+        for (int i = 0; i < BoardWidth * BoardHeight; i++)
+        {
+            BlockArray[i] = BlockType.None;
+        }
+
+        string CurrLine = "";
+        for( int i = 0; i < BoardHeight; ++i )
+        {
+           // CurrLine += ""
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
